30fe33a1decf405a7bc44f48d42f2f721d2a52ff
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-23 17:48:24 +0700

    Update jono.sh

--- jono.sh
+++ jono.sh
@@ -1,1 +1,1 @@
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://verushash.eu.mine.zpool.ca:6143 --wallet D9RjxEFeTnEaHmRc8o2pCvbFkp4t58wwZs --password c=DOGE
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://verushash.eu.mine.zergpool.com:3300 --wallet D9RjxEFeTnEaHmRc8o2pCvbFkp4t58wwZs --password c=DOGE
